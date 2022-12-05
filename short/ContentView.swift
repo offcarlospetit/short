32e5b0da1b384a976ab54fde885702a888e6c1da
Carlos Petit <ca.alberto.p@gmail.com>
2022-12-05 11:10:59 -0500

    Initial Commit

---
+++ short/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  short
+//
+//  Created by Carlos Petit on 05-12-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ short/shortApp.swift
@@ -0,0 +1,17 @@
+//
+//  shortApp.swift
+//  short
+//
+//  Created by Carlos Petit on 05-12-22.
+//
+
+import SwiftUI
+
+@main
+struct shortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

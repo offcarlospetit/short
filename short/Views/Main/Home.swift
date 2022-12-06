466f723915dfaf87aec0aabda42ae1b54ddff63a
Carlos Petit <ca.alberto.p@gmail.com>
2022-12-05 22:25:54 -0500

    wip: update tabview

--- short/Views/Main/Home.swift
+++ short/Views/Main/Home.swift
@@ -8,31 +8,68 @@
 import SwiftUI
 
 struct Home: View {
+    @State var selectedIndex = 0
     var body: some View {
         VStack{
             ZStack{
                 TabView {
                     Feed()
+                        .onTapGesture {
+                            self.selectedIndex = 0
+                        }
                         .tabItem {
                             Image(systemName: "house")
                                 .imageScale(.large)
+                                .foregroundColor(Color("BackGround"))
                         }
                     Search()
+                        .onTapGesture {
+                            self.selectedIndex = 1
+                        }
                         .tabItem {
                             Image(systemName: "magnifyingglass")
                                 .imageScale(.large)
                         }
                     Notificaations()
+                        .onTapGesture {
+                            self.selectedIndex = 2
+                        }
                         .tabItem {
                             Image(systemName: "bell")
                                 .imageScale(.large)
                         }
                     Messages()
+                        .onTapGesture {
+                            self.selectedIndex = 3
+                        }
                         .tabItem {
                             Image(systemName: "envelope")
                                 .imageScale(.large)
                         }
-                }            }
+                }
+                
+                .padding()
+                .accentColor(Color("BackGround"))
+                
+                
+                VStack{
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "pencil")
+                                .frame(width: 20, height: 20)
+                                .padding()
+                                .background(Color("BackGround"))
+                                .foregroundColor(.white)
+                                .clipShape(Circle())
+                        }
+                    }.padding()
+                }.padding(.bottom, 65)
+                
+            }
         }
     }
 }
